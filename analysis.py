2c36c2d6557ae7d50e36d2eb3cb17af1ee423944
Niall Wynne <nwynne93@gmail.com>
2023-04-13 13:40:01 +0100

    Module import

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 # A program to perform analysis on the iris data set
 
-import pandas as pd
+import pandas as pd # needed to read in data set
+import matplotlib as mp # will need for histogram
 
 data = pd.read_csv('iris_csv.csv') # referenced in readme
 
