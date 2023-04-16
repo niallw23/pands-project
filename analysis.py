0787a080e81ac90145e2e94007886188470e8040
Niall Wynne <nwynne93@gmail.com>
2023-04-16 19:16:48 +0100

    Setting up for 1st task

--- analysis.py
+++ analysis.py
@@ -1,7 +1,13 @@
 # A program to perform analysis on the iris data set
 
 import pandas as pd # needed to read in data set
-import matplotlib as mp # will need for histogram
+import matplotlib.pyplot as plt # will need for histogram
+import numpy as np # with view to my own analysis
 
 data = pd.read_csv('iris_csv.csv') # referenced in readme
+
+# first instruction
+# program should output a summary of each variable to a single text file
+
+
 
