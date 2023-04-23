33f27c6785b5285ea2d1fbb1822fdc7562d52b72
Niall Wynne <nwynne93@gmail.com>
2023-04-23 23:00:06 +0100

    second part - histogram initial

--- analysis.py
+++ analysis.py
@@ -4,16 +4,26 @@ import pandas as pd # needed to read in data set
 import matplotlib.pyplot as plt # will need for histogram
 import numpy as np # with view to my own analysis
 
-data = pd.read_csv('iris_csv.csv') # referenced in readme
+iris_data = pd.read_csv('iris_csv.csv') # referenced in readme
 
 # first instruction
 # program should output a summary of each variable to a single text file
 
-summary = data.describe() #uses describe() to get the summary, referenced in readme
+summary = iris_data.describe() #uses describe() to get the summary, referenced in readme
 
 # Write the summary statistics to a text file
 #with open('iris_summary.txt', 'w') as file:
 #    file.write(summary)  # got an error that argument must be string
 
-with open ('iris_summary.txt', 'w') as file:
-    file.write(summary.to_string())
+#with open ('iris_summary.txt', 'w') as file:
+    #file.write(summary.to_string())
+
+#second instruction
+#saves a histogram of each variable to png files
+
+for col in iris_data.columns[:-1]:
+    plt.hist(iris_data[col])
+    plt.title(col)
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.show()
